39dc6dbbf834be95172d426a696bd317bb5873d3
gregariousgovind <govind.chauhan@mountblue.io>
2022-01-05 00:59:50 +0530

    12 files modified

---
+++ src/components/MainHeader/Navigation.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './Navigation.scss';
+
+const Navigation = (props) => {
+  return (
+    <nav className="nav">
+      <ul>
+        {props.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {props.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {props.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
