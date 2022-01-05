8f99b4c33cb47d62807a28f4067a7b25a2d477ac
gregariousgovind <govind.chauhan@mountblue.io>
2022-01-05 22:21:35 +0530

    1 file modified

--- src/store/auth-context.tsx
+++ src/store/auth-context.tsx
@@ -29,7 +29,7 @@ export const AuthContextProvider = (props) => {
   return (
     <AuthContext.Provider
       value={{
-        isLoggedIn: false,
+        isLoggedIn: isLoggedIn,
         onLogout: logoutHandler,
         onLogin: loginHandler,
       }}
